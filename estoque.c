30f35152c55fc4721fea72635ba79fb703589d2a
marlem <marlem.magno@gmail.com>
2021-02-08 20:17:16 -0300

    3/15

--- estoque.c
+++ estoque.c
@@ -11,86 +11,69 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 
 /// 
 /// Assinaturas das funções
 ///
-void menuSobre(void);
-void menuPrincipal(void);
 
-void menuProduto(void);
+char menuPrincipal(void);
+
+char menuProduto(void);
 void telaCadastrarNovoProduto(void);                              	  
 void telaPesquisarDadosDeUmProduto(void);                      	
 void telaAtualizarProduto(void);                                      	  
 void telaExcluirUmProdutoDoSistema(void); 
 
-void menuAtualizar(void);
+char menuAtualizar(void);
 void telaAlterarNomedoProduto(void);                             	  
 void telaAlterarCodigodeBarra(void);                             	  
 void telaAlterarValidade(void);                                       	 
 void telaAlterarValor(void);  
 
-void telaInventario(void);
+char telaInventario(void);
 void menuSaidaproduto(void);
 void menuEntradaproduto(void)
 void telaRelatorio(void);
 
+void menuSobre(void);
 
 ///
 /// Programa Principal
 ///
+
 int main(void) {
+	char opcao1, opcoa2, opcao3, opcao4, opcao5, opcao6;
+
+
     menuSobre();
-    menuPrincipal();
-    menuProduto();
-	telaCadastrarNovoProduto();                              	  
-	telaPesquisarDadosDeUmProduto();                      	
-	telaAtualizarProduto();                                      	  
-	telaExcluirUmProdutoDoSistema(); 
-    menuAtualizar();
-	telaAlterarNomedoProduto();                             	  
-	telaAlterarCodigodeBarra();                             	  
-	telaAlterarValidade();                                       	 
-	telaAlterarValor
-    telaInventario();
-    menuSaidaproduto();
-	telaRelatorio();
-	menuEntradaproduto()
+	opcao1 = menuPrincipal();
+
+	opcoa2 = menuProduto();
+	telaCadastrarNovoProduto();
+	telaPesquisarDadosDeUmProduto();
+	telaAtualizarProduto();
+	telaExcluirUmProdutoDoSistema();
+
+	opcao3 = menuAtualizar();
+	telaAtualizarProduto();
+	telaAlterarCodigodeBarra();
+	telaAlterarValidade();
+	telaAlterarValor();
+
+	opcao4 = menuSaidaproduto();
+	
+	opcao5 = menuEntradaproduto();
+
+	opcao6 = telaRelatorio();
+
     return 0;
 
-    }
-
-
-void menuSobre(void) {
-    printf("\n");
-    printf("/////////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                           ///\n");
-    printf("///              Universidade Federal do Rio Grande do Norte                  ///\n");
-    printf("///                  Centro de Ensino Superior do Seridó                      ///\n");
-    printf("///                Departamento de Computação e Tecnologia                    ///\n");
-    printf("///                   Disciplina DCT1106 -- Programação                       ///\n");
-    printf("///                 Projeto Sistema de Controle de Estoque                    ///\n");
-    printf("///             Developed by  @dleonidas200 @MarlemM - Jan, 2021              ///\n");
-    printf("///                                                                           ///\n");
-    printf("/////////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                           ///\n");
-    printf("///       = = = = =      Sistema de Controle de Estoque       = = = = =       ///\n");
-    printf("///                                                                           ///\n");
-    printf("///   As empresas da atualidade utilizam complexos sistemas gerenciais para,  ///\n");
-    printf("///   entre várias outras funções, gerenciar seu estoque de produtos. Geren-  ///\n");
-    printf("///   ciar estoques de maneira eficiente, sem a ajuda de um sistema automa-   ///\n");
-    printf("///   tizado, é algo difícil para as pequenas empresas e impossível para      ///\n");
-    printf("///   as grandes. O presente projeto tem como objetivo propor um software     ///\n");
-    printf("///   que ofereça funcionalidades básicas relacionadas ao controle de inven-  ///\n");
-    printf("///   tário de uma empresa, visando facilitar a administração de estoques     ///\n");
-    printf("///   de produtos dos mais diversos tipos.                                    ///\n");
-    printf("///                                                                           ///\n");
-    printf("/////////////////////////////////////////////////////////////////////////////////\n");
-
-
-void menuPrincipal(void) {
-
+}
+char menuPrincipal(void) {
+	char opcao;
+	
     system("clear");
 	printf("\n");
 	printf("/////////////////////////////////////////////////////////////////////////////////\n");
@@ -121,13 +104,19 @@ void menuPrincipal(void) {
 	printf("///                                                                     	  ///\n");
 	printf("///                                                                     	  ///\n");
 	printf("/////////////////////////////////////////////////////////////////////////////////\n");
-	printf("\n");
-    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+	printf("Qual a sua opção:");
+	scanf("%c", opcao);
 	getchar();
+	rintf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+		getchar();
+		return opcao;
     
 }
 
-void menuProduto(void) {
+char manuProdutos(void){
+	char opcao;
+
     system("clear");
 	printf("\n");
 	printf("/////////////////////////////////////////////////////////////////////////////////\n");
@@ -155,12 +144,23 @@ void menuProduto(void) {
 	printf("///                                                                      	  ///\n");
 	printf("///                                                                     	  ///\n");
 	printf("/////////////////////////////////////////////////////////////////////////////////\n");
-	printf("\n");
+	printf("Qual a sua opção:\n");
+	scanf("%c", &opcao);
+	getchar("\n");
     printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
-	getchar();
+		getchar();
+		return opcao;
 }
 
+
+
 void telaCadastrarNovoProduto(void){
+
+	char codBarras[14];
+	char nomeItem[51];
+	char dataValidade[11];
+	
+//tirar a parte de quantidade da tela
 	system("clear");
 	printf("/////////////////////////////////////////////////////////////////////////////////\n");
 	printf("///                                                                           ///\n");
@@ -174,9 +174,15 @@ void telaCadastrarNovoProduto(void){
     printf("///            *Cadastrar Novo Produto:                              		  ///\n");
     printf("///                                                                    		  ///\n");
     printf("///            - Código de Barras:                                     		  ///\n");
-    printf("///            - Nome:                                              	   	  ///\n");
-    printf("///            - Data de Validade:                                  		  ///\n");
-    printf("///            - Quantidade:                                        		  ///\n");
+   	scanf("%[0-9]", codBarras);
+    getchar();
+	printf("///            - Nome:                                              	   	  ///\n");
+    scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ]", nomeItem);
+	getchar();
+	printf("///            - Data de Validade:                                  		  ///\n");
+    scanf("%[0-9/]", dataValidade);
+	getchar();
+	//printf("///            - Quantidade:                                        		  ///\n");
     printf("///        _______________________________________________________     		  ///\n");
     printf("///                                                                 		  ///\n");
     printf("///          Deseja adicionar um novo produto (S/n) ?                 		  ///\n");
@@ -185,7 +191,13 @@ void telaCadastrarNovoProduto(void){
     printf("///         Produto cadastrado com sucesso !                           		  ///\n");
     printf("///        _______________________________________________________  		  ///\n");
     printf("///                                                                 		  ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n\n");
+    printf("///              Código de Barras: %s \n", codBarras);
+    printf("///                     Descrição: %s \n", nomeItem);
+    printf("///              Data de Validade: %s \n", dataValidade);
+    printf("///        ___________________________________________________        ///\n");
+    printf("///                                                                   ///\n");
+    printf("///                >>> Tecle <ENTER> para continuar...                ///\n");
+	printf("///////////////////////////////////////////////////////////////////////////////\n\n");
     printf("\t\t>>> Tecle <ENTER> para continuar...\n");
     getchar();
 }
@@ -204,14 +216,15 @@ void telaPesquisarDadosDeUmProduto(void){
     printf("///            *Pesquisar Dados de Um Produto:                       		  ///\n");
     printf("///                                                                 		  ///\n");
     printf("///            - Codigo de Barras:                                  		  ///\n");
-    printf("///        _______________________________________________________      	  ///\n");
+    scanf("%[0-9]", codBarras);
+	printf("///        _______________________________________________________      	  ///\n");
     printf("///                                                                 		  ///\n");
     printf("///            *Produto Encontrado!                                  		  ///\n");
     printf("///                                                                 		  ///\n");
     printf("///            - Codigo de Barras:                                  		  ///\n");
     printf("///            - Nome:                                              		  ///\n");
     printf("///            - Data de Validade:                                  		  ///\n");
-    printf("///            - Quantidade:                                        		  ///\n");
+    //printf("///            - Quantidade:                                        		  ///\n");
     printf("///        _______________________________________________________  		  ///\n");
     printf("///                                                                 		  ///\n");  
     printf("///            Não foi possível encontrar produto cadastrado com o     		  ///\n");    
@@ -221,9 +234,13 @@ void telaPesquisarDadosDeUmProduto(void){
     printf("///////////////////////////////////////////////////////////////////////////////\n\n");
     printf("\t\t>>> Tecle <ENTER> para continuar...\n");
     getchar();
+
 }
 
 void telaAtualizarProduto(void); 
+	char codBarras[14];
+    char nomeItem[51];
+    char dataValidade[11];
 	system("clear");
 	system("clear");
     printf("/////////////////////////////////////////////////////////////////////////////////\n");
@@ -241,6 +258,9 @@ void telaAtualizarProduto(void);
 
 
 void telaExcluirUmProdutoDoSistema(void);
+	char codBarras[14];
+	char resposta[2];
+
 	system("clear");
     printf("/////////////////////////////////////////////////////////////////////////////////\n");
 	printf("///                                                                           ///\n");
@@ -254,6 +274,7 @@ void telaExcluirUmProdutoDoSistema(void);
     printf("///            *Excluir Um Produto do Sistema:                       		  ///\n");
 	printf("///                                                                           ///\n");
     printf("///            - Código de Barras:                                 		      ///\n");
+   scanf("%[0-9]", codBarras);
     printf("///        _______________________________________________________      	  ///\n");
     printf("///                                                                 		  ///\n");
     printf("///            PRODUTO LOCALIZADO!                                  		  ///\n");
@@ -269,12 +290,14 @@ void telaExcluirUmProdutoDoSistema(void);
     printf("///        _______________________________________________________      	  ///\n");
     printf("///                                                                 		  ///\n");
     printf("///            - Confirmar operação (S/n): S                        		  ///\n");
-    printf("///                                                                 		  ///\n");
+    scanf("%[SNsn]", resposta);
+	printf("///                                                                 		  ///\n");
     printf("///            Produto excluído com sucesso!                        		  ///\n");
     printf("///        _______________________________________________________      	  ///\n");
     printf("///                                                                 		  ///\n");
     printf("///            - Confirmar operação (S/n): N                        		  ///\n");
-    printf("///                                                                 		  ///\n");
+    scanf("%[SNsn]", resposta);
+	printf("///                                                                 		  ///\n");
     printf("///            Operação cancelada!                                  		  ///\n");
     printf("///        _______________________________________________________      	  ///\n");
     printf("///                                                                 		  ///\n");
@@ -432,7 +455,7 @@ void telaInventario(void) {
 	printf("\n");
     printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
 	getchar();
-
+}
 	void menuEntradaproduto(void) {
     system("clear");
 	printf("\n");
@@ -491,7 +514,33 @@ void telaInventario(void) {
 	printf("///                                                                     	  ///\n");
 	printf("/////////////////////////////////////////////////////////////////////////////////\n");
 	getchar();
-
+}
+void menuSobre(void) {
+    printf("\n");
+    printf("/////////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                           ///\n");
+    printf("///              Universidade Federal do Rio Grande do Norte                  ///\n");
+    printf("///                  Centro de Ensino Superior do Seridó                      ///\n");
+    printf("///                Departamento de Computação e Tecnologia                    ///\n");
+    printf("///                   Disciplina DCT1106 -- Programação                       ///\n");
+    printf("///                 Projeto Sistema de Controle de Estoque                    ///\n");
+    printf("///             Developed by  @dleonidas200 @MarlemM - Jan, 2021              ///\n");
+    printf("///                                                                           ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                           ///\n");
+    printf("///       = = = = =      Sistema de Controle de Estoque       = = = = =       ///\n");
+    printf("///                                                                           ///\n");
+    printf("///   As empresas da atualidade utilizam complexos sistemas gerenciais para,  ///\n");
+    printf("///   entre várias outras funções, gerenciar seu estoque de produtos. Geren-  ///\n");
+    printf("///   ciar estoques de maneira eficiente, sem a ajuda de um sistema automa-   ///\n");
+    printf("///   tizado, é algo difícil para as pequenas empresas e impossível para      ///\n");
+    printf("///   as grandes. O presente projeto tem como objetivo propor um software     ///\n");
+    printf("///   que ofereça funcionalidades básicas relacionadas ao controle de inven-  ///\n");
+    printf("///   tário de uma empresa, visando facilitar a administração de estoques     ///\n");
+    printf("///   de produtos dos mais diversos tipos.                                    ///\n");
+    printf("///                                                                           ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////////////\n");
+}
 
     printf("/////////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                           ///\n");
