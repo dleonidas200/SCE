517112b3d5edee215d6dd849f4e0ea09991b497a
dleonidas200 <77741714+dleonidas200@users.noreply.github.com>
2021-04-07 15:38:26 -0300

    conserto

--- moduloMain.h
+++ moduloMain.h
@@ -1,1 +1,8 @@
 char menuPrincipal(void);
+void telaCadastrarNovoProduto(void);
+void menuSaidaproduto(void);
+void telaAtualizarProduto(void);
+void telaRelatorio(void);
+void telaPesquisarDadosDeUmProduto(void);
+void menuSobre(void);
+void telaExcluirUmProdutoDoSistema(void); 
