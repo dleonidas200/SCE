90f48f62744e19b0d109578616527acc9d30d074
dleonidas200 <pfilho200@gmail.com>
2021-05-06 18:37:26 -0300

    15/15

--- moduloSaida.h
+++ moduloSaida.h
@@ -1,6 +1,6 @@
 void moduloSaida(void);
 char menuSaida(void);
-char* telaUnformarSaida(void);
+char* telaInformarSaida(void);
 void realizarSaida(void);
 int fazerRetirada(char* codBarras);
 int pegarQuantidadeRetirada(void);
